8d352f014e6f1781641160ef1154cb12923af659
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-04-19 13:37:03 +0530

    Create Home.js

---
+++ React Router/LECTURE 25/Components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home =()=>
+{
+    return(
+        <div className="container">
+        <h4 className="center">Home</h4>
+        <p>hello this is avinash soni. i am a engineering student and i am not having corona virus </p>
+        
+        </div>
+    )
+}
+export default Home
