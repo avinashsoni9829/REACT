34173e765ebb1aa30db84f6b372c86a078155756
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-04-19 15:59:35 +0530

    Create [updated] About.js

---
+++ React Router/hoc/[updated] About.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+const About =()=>
+{
+    return(
+        <div className="container">
+        <h4 className="center">About</h4>
+        <p>hello this is avinash soni. i am a engineering student and i am not having corona virus </p>
+        
+        </div>
+    )
+}
+export default Rainbow(About)
