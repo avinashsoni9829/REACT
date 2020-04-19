77a98bdfd5d6f7d2d752e9c285e33c3e4a1d56d2
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-04-19 12:23:09 +0530

    COMPLETING THE TODO APP

---
+++ TodoApp/LECTURE 24/App.js
@@ -0,0 +1,46 @@
+import React,{Component} from 'react';
+import Todos from './Todos'
+import AddTodo from './AddTodo'
+
+class App extends Component
+{
+  state={
+    todos: [
+      {id:1,content:'buy some milk'},
+      {id:2,content:'play mario kart'}
+    ]
+  }
+  deleteTodo=(id)=>
+  {
+    const todos=this.state.todos.filter(todo=>{
+      return todo.id!==id
+    });
+
+    this.setState({
+      todos
+    })
+  }
+
+  addTodo=(todo) =>{
+    todo.id =Math.random();
+    //we cant directly alter the todo array so we need to create a new array to make changes to it 
+
+    let todos = [...this.state.todos,todo];  // . . . is the spread operator 
+    this.setState({
+      todos
+    })
+
+  }
+  render()
+  {
+    return(
+      <div className="todo-app container">
+      <h1 className="center blue-text">Todo's</h1>
+      <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+      <AddTodo addTodo={this.addTodo} />
+      </div>
+    );
+  }
+}
+
+export default App;
