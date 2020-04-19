b1608c6b59d1e21d0805306d4feb49a1ddc22411
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-04-19 13:37:37 +0530

    Create Contact.js

---
+++ React Router/LECTURE 25/Components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact =()=>
+{
+    return(
+        <div className="container">
+        <h4 className="center">Contact</h4>
+        <p>hello this is avinash soni. i am a engineering student and i am not having corona virus </p>
+        
+        </div>
+    )
+}
+export default Contact
