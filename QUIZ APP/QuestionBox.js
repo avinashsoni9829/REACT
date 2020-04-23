6731d797453df5e35694291c4dcd90f4b22520b8
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-04-23 13:22:04 +0530

    Update QuestionBox.js

--- QUIZ APP/QuestionBox.js
+++ QUIZ APP/QuestionBox.js
@@ -1,24 +1,31 @@
-import React,{ useState } from "react";
+import React ,{useState} from "react";
 
-const QuestionBox = ({ question,options}) => 
+
+const QuestionBox= ({question,options,selected }) =>   //props: question,options ,selected
 {
-    const [answer, setAnswer] = useState(options);
-   
-    return
-    (
+
+    const [answer,setAnswer] =useState(options);
+
+    return (
         <div className="questionBox">
         <div className="question">{question}</div>
-       {answer.map((text, index)=>
-        (
-            <button key={index} className="answerBtn" onClick={() =>{
-                setAnswer([text]);
+        {answer.map((text,index)=> (
+            <button
+             key={index} 
+             className="answerBtn"
+              onClick={() =>{
+                 setAnswer([text]);
+                 selected(text);
 
-            }}>{text}</button>
+
+            }}
+            >
+            {text}
+             </button>
         ))}
-       
         </div>
     );
 };
 
-
 export default QuestionBox;
+
