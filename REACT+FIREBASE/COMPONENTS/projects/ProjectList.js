298235f457275776695854ebc8f1ba3030215333
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-05-14 16:55:20 +0530

    Add files via upload

---
+++ REACT+FIREBASE/COMPONENTS/projects/ProjectList.js
@@ -0,0 +1,40 @@
+//functional component
+//data gonna flow in from dashboard component as a prop in this 
+
+import React from 'react'
+
+const ProjectList = () =>
+{
+    return(
+        <div className="project-list section">
+        
+        <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>posted by the net ninja</p>
+        <p className="grey-text">3rd September ,2AM</p>
+        </div>
+        </div>
+
+        <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>posted by the net ninja</p>
+        <p className="grey-text">3rd September ,2AM</p>
+        </div>
+        </div>
+        <div className="card z-depth-0 project-summary">
+        <div className="card-content grey-text text-darken-3">
+        <span className="card-title">Project Title</span>
+        <p>posted by the net ninja</p>
+        <p className="grey-text">3rd September ,2AM</p>
+        </div>
+        </div>
+        
+        
+        </div>
+    )
+}
+
+
+export default ProjectList
