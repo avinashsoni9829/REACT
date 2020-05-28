715e2a0aa64806f4021e532463f22eaf2b4e1bf1
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-05-28 12:32:24 +0530

    Update SignedOutLinks.js

--- REACT+FIREBASE/COMPONENTS/LAYOUT/SignedOutLinks.js
+++ REACT+FIREBASE/COMPONENTS/LAYOUT/SignedOutLinks.js
@@ -1,18 +1,14 @@
-//this component is  not going to have a state thats why it can be fucntional component instead of a class component 
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-const SignedOutlinks = () =>
-{
-    return (
-        <ul className="right">
-        <li> <NavLink to='/'>Signup</NavLink></li>
-        <li> <NavLink to='/'>Login</NavLink></li>
-        
-        
+import React from 'react';
+import {NavLink} from 'react-router-dom'; //allows use of <NavLink> tag. Use of active link?
+
+
+const SignedOutLinks = () => {
+    return(
+        <ul className="right"> {/*classname materialize sets links to right side */}
+            <li><NavLink to='/signup'>Signup</NavLink></li> {/*using routes set up in App.js */}
+            <li><NavLink to='/signin'>Login</NavLink></li>
         </ul>
-        
     )
 }
 
-
-export default SignedOutlinks
+export default SignedOutLinks;
