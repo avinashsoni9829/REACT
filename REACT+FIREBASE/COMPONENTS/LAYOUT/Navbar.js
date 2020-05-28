5c1b1b6a0e42753c9cfe6f2e9bc32a8f834bcc82
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-05-28 12:31:33 +0530

    Update Navbar.js

--- REACT+FIREBASE/COMPONENTS/LAYOUT/Navbar.js
+++ REACT+FIREBASE/COMPONENTS/LAYOUT/Navbar.js
@@ -1,24 +1,32 @@
-//this component is  not going to have a state thats why it can be fucntional component instead of a class component 
-import React from 'react'
-import { Link } from 'react-router-dom'
-import SignedInLinks from './SignedInLinks'
-import SignedOutLinks from './SignedOutLinks'
+import React from 'react';
+import {Link} from 'react-router-dom'; //allows use of <Link> tag. react docs. provides navigation around app.
+import SignedInLinks from './SignedInLinks';
+import SignedOutLinks from './SignedOutLinks';
+import {connect} from 'react-redux'; //for export
 
-
-const Navbar = () =>
-{
-    return (
-        <nav className ="nav-wrapper grey darken-3">
-        <div className="container">
-        <Link to='/' className="brand-logo">MarioPlan</Link>
-        <SignedInLinks/>
-        <SignedOutLinks/>
-
-        </div>
-        </nav> 
-
+const Navbar = (props) => {
+    const {auth, profile} = props;
+    // console.log(auth);
+    const links = auth.uid ? <SignedInLinks profile={profile}/> : <SignedOutLinks />//if auth.uid exists ie user is signed in show signedinlinks, else signedoutLinks
+    return(
+        <nav className="nav.nav-wrapper grey darken-3"> {/*darken by 3 shades (materialize class)*/}
+            <div className="container">
+                <Link to='/' className='brand-logo'>MarioPlan</Link> {/*/when clicked redirects to home. brand-logo-> materialize.css*/}
+                {links} {/* ternary operator above to show or hide signin links */}
+            </div>
+         </nav>   
     )
 }
 
-
-export default Navbar
+//video #22/ #25 log out
+//convert state to props for component
+const mapStateToProps = (state) =>
+{
+    
+    console.log(state); //firebase ifEmpty: true if not logged in
+    return{
+        auth: state.firebase.auth,
+        profile: state.firebase.profile //video #29 profile from index.js rrf.props
+    }
+}
+export default connect(mapStateToProps) (Navbar);
