fee4d763098a103e38bb393c7d065c7528d71f0a
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-05-28 12:48:30 +0530

    sign in sign up

--- REACT+FIREBASE/COMPONENTS/AUTH/SignIn.js
+++ REACT+FIREBASE/COMPONENTS/AUTH/SignIn.js
@@ -1,76 +1,64 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux'; //video #23
-import {signIn} from '../../store/actions/authActions'; //connect with actions from dispatch
-import {Redirect} from 'react-router-dom'; //video #27 route guarding
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-class Signin extends Component
-{
+import { signIn } from '../../store/actions/authActions';
+
+export class SignIn extends Component {
     state = {
-        email: "",
-        password: ""
+        email: '',
+        password: ''
     }
-
-    //React e for event
-    handleChange = (e) => 
-    {
-        // console.log(e.timeStamp); time since last change ms
-        console.log(e);
-
-        //this will save the state for whichever input id is being altered. email or password
-        this.setState({
+    
+    handleChange = (e) => {
+        this.setState({ 
             [e.target.id]: e.target.value
-        })
+        });
     }
 
-    handleSubmit = (e) =>
-    {
-        e.preventDefault(); // prevent default reload of page on submission
-        // console.log(this.state); //log submitted email and password to console
-        this.props.signIn(this.state); //state is email and pass to authActions
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.signIn(this.state);
     }
-    render()
-    {
-        const {authError, auth} = this.props;
-        //redirects if user IS signed in video #27
-        if (auth.uid)
-        {
-            return(<Redirect to='/' />)
-        }
-        return(
+
+    render() {
+        const { authError, auth } = this.props;
+        if (auth.uid) return <Redirect to='/' />
+        return (
             <div className="container">
-                <form onSubmit={this.handleSubmit} className="white">   {/**No parenthesis handleSubmit() so it will be fired on change not by page load */}
-                    <h5 className="grey-text text-darken-3">Sign In</h5>
-                    <div className="input-field">                       {/*materialize css */}
-                        <label htmlFor="email">Email</label>            {/*htmlFor specifies which form element label belongs to*/}
-                        <input type="email" id="email" onChange={this.handleChange}/>
-                    </div>
-                    <div className="input-field">
-                        <label htmlFor="password">Password</label>      {/*htmlFor specifies which form element label belongs to*/}
-                        <input type="password" id="password" onChange={this.handleChange}/>
-                    </div>     
-                    <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Login</button>
-                        <div className = "red-text" center>
-                            {authError ? <p>{authError}</p> : null} {/**if authError = null = false. authError from authAction->authReducer*/}
-                        </div>
-                    </div>       
-                </form>
+              <form onSubmit={this.handleSubmit} className="white">
+                <h5 className="grey-text text-darken-3">Sign In</h5>
+                <div className="input-field">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" id="email" onChange={this.handleChange} />
+                </div>
+                <div className="input-field">
+                  <label htmlFor="password">Password</label>
+                  <input type="password" id="password" onChange={this.handleChange} />
+                </div>
+                <div className="input-field">
+                  <button className="btn pink lighten-1 z-depth-0">Login</button>
+                  <div className="red-text center">
+                    { authError ? <p>{ authError }</p> : null }
+                  </div>
+                </div>
+              </form>
             </div>
-        )
+        );
     }
 }
 
-//video #23
 const mapStateToProps = (state) => {
-    return {
-        authError: state.auth.authError, //state.auth for root reducer, authError for authReducer 
-        auth: state.firebase.auth
-    }
+  return {
+    authError: state.auth.authError,
+    auth: state.firebase.auth
+  }
 }
-const mapDispatchToProps = (dispatch) =>
-{
-    return{
-        signIn: (creds) => dispatch(signIn(creds)) //creds from authActions
-    }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (creds) => dispatch(signIn(creds))
+  }
 }
-export default connect(mapStateToProps, mapDispatchToProps)(Signin); //connect(mapState,mapDispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
--- REACT+FIREBASE/COMPONENTS/AUTH/SignUp.js
+++ REACT+FIREBASE/COMPONENTS/AUTH/SignUp.js
@@ -1,89 +1,75 @@
-import React, {Component} from 'react';
-import {Redirect} from 'react-router-dom'; //video #27 route guarding
-import {connect} from 'react-redux';
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 
-import {signUp} from '../../store/actions/authActions'; //video #28 signup
+import { signUp } from '../../store/actions/authActions';
 
-//note, password must be longer than 6 chars
-class SignUp extends Component
-{
+export class SignUp extends Component {
     state = {
-        email: "",
-        password: "",
-        firstName:"",
-        LastName:""
+        email: '',
+        password: '',
+        firstName: '',
+        lastNae: ''
     }
-
-    //React e for event
-    handleChange = (e) => 
-    {
-        // console.log(e.timeStamp); time since last change ms
-        console.log(e);
-
-        //this will save the state for whichever input id is being altered. email or password
-        this.setState({
+    
+    handleChange = (e) => {
+        this.setState({ 
             [e.target.id]: e.target.value
-        })
+        });
     }
 
-    handleSubmit = (e) =>
-    {
-        e.preventDefault(); // prevent default reload of page on submission
-        // console.log(this.state); //log submitted email and password to console
-        this.props.signUp(this.state); //send new user state data into signUp dispatch
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.signUp(this.state);
     }
-    render()
-    {
-        //route guard video #27
-        const {auth, authError} = this.props;
-        if (auth.uid)
-        {
-            return(<Redirect to='/' />)
-        }    
-        return(
+
+    render() {
+      const { auth, authError } = this.props;
+      if (auth.uid) return <Redirect to='/' />
+
+        return (
             <div className="container">
-                <form onSubmit={this.handleSubmit} className="white">   {/**No parenthesis handleSubmit() so it will be fired on change not by page load */}
-                    <h5 className="grey-text text-darken-3">Sign Up</h5>
-                    <div className="input-field">                       {/*materialize css */}
-                        <label htmlFor="email">Email</label>            {/*htmlFor specifies which form element label belongs to*/}
-                        <input type="email" id="email" onChange={this.handleChange}/>
-                    </div>
-                    <div className="input-field">
-                        <label htmlFor="password">Password</label>      {/*htmlFor specifies which form element label belongs to*/}
-                        <input type="password" id="password" onChange={this.handleChange}/>
-                    </div>     
-                    <div className="input-field">
-                        <label htmlFor="firstName">First Name</label>      {/*htmlFor specifies which form element label belongs to*/}
-                        <input type="text" id="firstName" onChange={this.handleChange}/>
-                    </div>     
-                    <div className="input-field">
-                        <label htmlFor="lastName">Last Name</label>      {/*htmlFor specifies which form element label belongs to*/}
-                        <input type="text" id="lastName" onChange={this.handleChange}/>
-                    </div>     
-                    <div className="input-field">
-                        <button className="btn pink lighten-1 z-depth-0">Submit</button>
-                        <div className = "red-text" center>
-                            {authError ? <p>{authError}</p> : null} {/**if auth fails print to screen from firebase. authError from authAction->authReducer*/}
-                        </div>    
-                    </div>       
-                </form>
+              <form onSubmit={this.handleSubmit} className="white">
+                <h5 className="grey-text text-darken-3">Sign In</h5>
+                <div className="input-field">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" id="email" onChange={this.handleChange} />
+                </div>
+                <div className="input-field">
+                  <label htmlFor="password">Password</label>
+                  <input type="password" id="password" onChange={this.handleChange} />
+                </div>
+                <div className="input-field">
+                  <label htmlFor="firstName">First Name</label>
+                  <input type="text" id="firstName" onChange={this.handleChange} />
+                </div>
+                <div className="input-field">
+                  <label htmlFor="lastName">Last Name</label>
+                  <input type="text" id="lastName" onChange={this.handleChange} />
+                </div>
+                <div className="input-field">
+                  <button className="btn pink lighten-1 z-depth-0">Login</button>
+                  <div className="red-text center">
+                    { authError ? <p>{ authError }</p> : null}
+                  </div>
+                </div>
+              </form>
             </div>
         )
     }
 }
-//video #27
+
 const mapStateToProps = (state) => {
-    return {
-        auth: state.firebase.auth,
-        authError: state.auth.authError
-    }
+  return {
+    auth: state.firebase.auth,
+    authError: state.auth.authError
+  }
 }
-//video #28
-const mapDispatchToProps = (dispatch) =>
-{
-    return {
-        signUp: (newUser) => dispatch(signUp(newUser))
-    }
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (newUser) => dispatch(signUp(newUser))
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
