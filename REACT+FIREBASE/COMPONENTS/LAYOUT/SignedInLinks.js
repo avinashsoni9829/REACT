0c544196eed293cb78b8a068ab409cbef8d94c67
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-05-28 12:31:57 +0530

    Update SignedInLinks.js

--- REACT+FIREBASE/COMPONENTS/LAYOUT/SignedInLinks.js
+++ REACT+FIREBASE/COMPONENTS/LAYOUT/SignedInLinks.js
@@ -1,18 +1,31 @@
-//this component is  not going to have a state thats why it can be fucntional component instead of a class component 
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-const SignedInlinks = () =>
-{
-    return (
-        <ul className="right">
-        <li> <NavLink to='/'>New Project</NavLink></li>
-        <li> <NavLink to='/'>Logout</NavLink></li>
-        <li> <NavLink to='/' className='btn btn-floating pink lighten-1'>NN</NavLink></li>
-        
+import React from 'react';
+import {NavLink} from 'react-router-dom'; //allows use of <NavLink> tag. Use of active link?
+
+//video #24 logging out, we want to dispatch action creator here
+import {connect} from 'react-redux';
+import {signOut} from '../../store/actions/authActions';
+
+const SignedInLinks = (props) => {
+
+    return(
+        <ul className="right"> {/*classname materialize sets links to right side */}
+            <li><NavLink to='/create'>New Project</NavLink></li>
+            <li><a onClick = {props.signOut}>Log Out</a></li> {/*anchortag <a>, and props.signOut no () so not to fire when page loads*/}
+            <li><NavLink to='/' className='btn btn-floating pink lighten -1'>{props.profile.initials}</NavLink></li> {/*styled to be a circle with materialize. video #29 props.initls seen in console when signed in*/}
         </ul>
-        
     )
 }
 
+//video #24 allow this component to access dispatch in redux
+//dispatch action creator in this component
 
-export default SignedInlinks
+//when Log Out is clicked, signOut function dispatches action import signOut which calls authAction signout that signs user out, which links to authReducer SIGNOUT_SUCCESS
+const mapDispatchToProps = (dispatch) => {
+    //returns object with signOut function
+    return{
+        signOut: () => dispatch(signOut()) //dispatch(action creator)
+    }
+}
+
+//connect to redux
+export default connect(null, mapDispatchToProps)(SignedInLinks);
