c3418998e48e08a789c1b0337e4630c5d1b6ef9d
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-06-06 12:41:16 +0530

    Create index.js

---
+++ REACT THEORY/TASKS/TASK-1/index.js
@@ -0,0 +1,13 @@
+//objective: render a <ul> to the page. this list should have 3 li in them
+
+import React from 'react';
+import ReactDOM  from 'react-dom';
+
+ReactDOM.render(
+    <ul>
+    <li>avinash soni </li>
+    <li>jecrc college </li>
+    <li>rtu </li>
+    </ul>,
+    document.getElementById('root')
+);
