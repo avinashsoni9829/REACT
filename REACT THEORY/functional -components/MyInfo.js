45cfc7eb7e2bb094f90bee7b4f58dc7078049e62
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-06-06 12:45:15 +0530

    Create MyInfo.js

---
+++ REACT THEORY/functional -components/MyInfo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function MyInfo()
+{
+    return(
+        <div>
+        <h1>hi this is avinash </h1>
+        <p> hello i am a programmer </p>
+        <ul>
+        <li>kashmir</li>
+        <li>london</li>
+        <li>mumbai</li>
+        </ul>
+        
+        </div>
+    )   
+}
+
+export default MyInfo;
+© 2020 GitHub, Inc.
