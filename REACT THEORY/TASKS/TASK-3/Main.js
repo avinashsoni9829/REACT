b6b83041c2f89f08d7244db57aceb47a2c617503
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-06-06 12:48:08 +0530

    Create Main.js

---
+++ REACT THEORY/TASKS/TASK-3/Main.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import b from "./components/b.js"
+import c from "./components/c.js"
+
+function Main()
+{
+    return(
+        <div>
+          <a/>
+        
+          <b/>
+        
+           <c/>
+          
+          </div>
+    )
+}
+
+export default Main;
