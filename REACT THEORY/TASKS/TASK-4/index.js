aacdf5a75353c6a31428e69d74bb5acbf83ce862
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-06-06 14:02:16 +0530

    Objective

---
+++ REACT THEORY/TASKS/TASK-4/index.js
@@ -0,0 +1,18 @@
+/*
+objective:
+
+render App component
+
+render 5 joke component
+
+joke should have question ,punchline prop
+
+ */
+
+
+import React from 'react';
+import ReactDOM from 'react-dom'
+import App from './App.js'
+
+
+ReactDOM.render(<App/>,document.getElementById('root'));
