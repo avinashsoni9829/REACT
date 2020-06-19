b300590f3e1d742417014a5119d1dedefe8f8cac
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-06-19 20:10:49 +0530

    Create vschoolProducts.js

---
+++ REACT THEORY/mapping components/vschoolProducts.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id:"1",
+        name:"Pencil",
+        price:1,
+        description:"perfect"
+        
+    },
+    {
+        id:"2",
+        name:"housing",
+        price:0,
+        description:" picture perfect"
+        
+    },
+    {
+        id:"3",
+        name:"housing dept",
+        price:10,
+        description:" extra picture perfect"
+        
+    }
+]
+
+
