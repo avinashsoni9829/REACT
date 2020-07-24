d990dbcf155a8df031dd53fdaf8d1f6b32086276
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2020-07-24 17:59:08 +0530

    Create app.js

---
+++ REACT THEORY/api fetch/app.js
@@ -0,0 +1,36 @@
+import React,{Component} from "react"
+
+class App extends Component{
+    constructor()
+    {
+        super()
+        this.state={
+            loading: false,
+            character: []
+        }
+    }
+    componentDidMount()
+    {    
+        this.setState({loading: true})
+        fetch("https://swapi.co/api/people/1")
+        .then(responce =>responce.json())
+        .then(data => {
+            this.setState({
+                loading:false,
+                character: data
+            })
+        })
+    }
+    
+    render()
+    {
+        const text=this.state.loading ? "loading..." : this.state.character.name
+        return(
+            <div>
+            <p>{text}</p>
+            </div>
+        )
+    }
+}    
+
+export default App
