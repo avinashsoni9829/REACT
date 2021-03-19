3a2312a64628f516d9c99367b2b3c02866e510c9
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-03-19 11:53:54 -0700

    Add files via upload

---
+++ theory/src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+import Greet from './components/Greet'
+import Hello from './components/hello'
+import Welcome from './components/Welcome'
+import Message from './components/Message'
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBinding from './components/EventBinding';
+import ParentComponent from './components/ParentComponent';
+import UserGreetings from './components/UserGreetings';
+import NameList from './components/NameList';
+
+
+
+function App() {
+  return (
+    <div className="App">
+   
+   <NameList/>
+    
+       
+       
+
+    </div>
+  );
+}
+
+export default App;
