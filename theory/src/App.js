d7b2df026df024158b66ac49c05ea857e8b819ca
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-04-07 01:02:31 +0530

    Add files via upload

--- theory/src/App.js
+++ theory/src/App.js
@@ -11,18 +11,38 @@ import EventBinding from './components/EventBinding';
 import ParentComponent from './components/ParentComponent';
 import UserGreetings from './components/UserGreetings';
 import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import Form from './components/Form';
+import LifecycleA from './components/LifecycleA';
+import FragmentDemo from './components/FragmentDemo';
+import Table from './components/Table';
+import PureComp from './components/PureComp';
+import parentComponent from './components/ParentComponent';
+import ParentComp from './components/ParentComp';
+import RefsDemo from './components/RefsDemo';
+import FocusInput from './components/FocusInput';
+import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
+import ClickCounterTwo from './components/ClickCounterTwo';
+import HoverCounterTwo from './components/HoverCounterTwo';
+import cnter from './components/cnter';
+import ComponentC from './components/ComponentC';
+import { UserProvider } from './components/UserContext';
+import PostList from './components/PostList';
+import PostForm from './components/PostForm';
 
 
 
 function App() {
   return (
     <div className="App">
-   
-   <NameList/>
+    <PostForm/>
     
-       
-       
-
     </div>
   );
 }
