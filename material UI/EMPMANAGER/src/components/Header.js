8b2d34d8c2e197809aabf3a93d4195898550efb7
avinashsoni9829 <45571070+avinashsoni9829@users.noreply.github.com>
2021-04-25 12:42:21 -0700

    Create Header.js

---
+++ material UI/EMPMANAGER/src/components/Header.js
@@ -0,0 +1,74 @@
+import {makeStyles, AppBar, Toolbar,Grid, InputBase, IconButton, Badge } from '@material-ui/core'
+import React from 'react'
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles= makeStyles(theme =>({
+    root:{
+        backgroundColor: '#fff',
+        // gives the shadow effect !
+        transform: 'translateZ(0)'
+    },
+    searchInput:{
+        opacity:'0.6',
+        padding:`0px ${theme.spacing(1)}px`,
+        fontSize:'0.8 rem',
+        '&:hover':{
+            backgroundColor:'#f2f2f2'
+        },
+        '& .MuiSvgIcon-root':{
+            marginRight:theme.spacing(1)
+         }
+    }
+}))
+
+
+export default function Header() {
+
+    const classes= useStyles();
+    
+    return (
+       <AppBar position="static" className={classes.root}>
+         
+         
+         <Toolbar>
+          
+           <Grid container
+                 alignItems="center">
+             <Grid item>
+                  <InputBase 
+                   placeholder="Search Here"
+                   className={classes.searchInput}
+                   startAdornment={<SearchIcon fontSize="small"/>}/>
+             </Grid>
+            
+             <Grid item sm></Grid>
+             <Grid item>
+                  
+                 <IconButton className={classes.btn}>
+                  <Badge badgeContent={4} color="secondary">
+                     <NotificationsNoneIcon/>
+                  </Badge>  
+                  </IconButton>
+
+                  <IconButton>
+                      <Badge badgeContent={4} color="primary">
+                      <ChatBubbleOutlineIcon/>
+                      </Badge> 
+                  </IconButton>
+                 
+                 <IconButton><PowerSettingsNewIcon/></IconButton>
+                 
+               
+                 
+             </Grid>
+              
+           </Grid>
+
+         </Toolbar>
+
+       </AppBar>
+    )
+}
